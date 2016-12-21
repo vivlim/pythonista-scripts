49f348e93eabc76ab39e17cbae945783d639f844
Vivian Lim <vivvnlim@gmail.com>
2016-12-21 08:14:50 +0000

    Change UploadImage default action to upload most recently taken photo/screenshot

--- UploadImage.py
+++ UploadImage.py
@@ -2,18 +2,18 @@ import appex
 import paramiko
 from datetime import datetime
 import clipboard
+import photos
 from PIL import Image
 
 max_image_size = 1024
 
 def main():
   if not appex.is_running_extension():
-    print('Running in Pythonista app, using test image...')
-    img = Image.open('test:Mandrill')
+    print('Running standalone, grabbing last picture created')
+    img = photos.get_recently_added_album().assets[-1].get_image()
   else:
     img = appex.get_image()
   if img:
-    # TODO: Your own logic here...
     print("You passed in an image of size {}x{}".format(img.width, img.height))
     if img.width > max_image_size or img.height > max_image_size:
         print("Scaling down image since it's big")
@@ -33,7 +33,7 @@ def main():
     filename = "upload-{}-{}-{}_{}-{}-{}.jpg".format(now.year, now.month, now.day, now.hour, now.minute, now.second)
     file = sftp.file("public_html/" + filename, mode="w")
     
-    img.save(file, "JPEG")
+    img.save(file, "JPEG", quality=100)
     file.flush()
     sftp.close()
     ssh.close()
